6caf4d7290eaddff273ae5dee9ed16c28b73597b
RK <rkehrli1993@gmail.com>
2022-08-18 01:49:25 -0400

    cleaned up metadata.

--- html/includes/assets/head.php
+++ html/includes/assets/head.php
@@ -5,11 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="robots" content="noindex" />
+    <meta name="author" content="RK" />
+    <meta name="description" content="AIRUGS is a new and upcoming Ai Art NFT Project." />
+
     <link rel="stylesheet" href="/includes/css/style.css">
     <link rel="shortcut icon" href="/includes/favicon.ico" type="image/x-icon">
-    <meta name="robots" content="noindex" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
+
     <script src="https://kit.fontawesome.com/7cd61cd6b1.js" crossorigin="anonymous"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
     <script src='/includes/scripts/header.js' type='text/javascript'></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
+
     <!--[if (lt IE 9)]><script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/min/tiny-slider.helper.ie8.js"></script><![endif]-->
