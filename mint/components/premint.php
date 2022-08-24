e153ca1fba334960c3bb0e7ec7be88ae34e5be98
RK <rkehrli1993@gmail.com>
2022-08-24 01:12:48 -0400

    Cleaned up countdown code and fixed typo.

--- mint/components/premint.php
+++ mint/components/premint.php
@@ -1,6 +1,6 @@
 <div class='premint-container'>
     <link rel="stylesheet" href="/mint/css/premint.css">
     <script src='/mint/scripts/countdown.js' type='text/javascript' defer></script>
-    <h1 class='textcenter'>Offical Launch</br>August 31<sup>th</sup>, 2022</h1>
+    <h1 class='textcenter'>Offical Launch</br>August 31<sup>st</sup>, 2022</h1>
     <div class="countdown"></div>
 </div>
--- mint/scripts/countdown.js
+++ mint/scripts/countdown.js
@@ -1,11 +1,13 @@
 // Initalize 
 $(document).ready(() => {
     const countdowntimer = new CountDownTimer('.countdown', 2022, 8, 31, 17, 0, 0);
-    // const countdowntimer = new CountDownTimer('.countdown', 2022, 8, 23, 22, 18, 0);
     countdowntimer.Render();
-})
-
-
+});
+/* 
+    Takes a launch date and will countdown until it's reached.
+    On reaching it will refresh the page and stop counting. 
+    Backend should switch components before refresh. 
+*/
 class CountDownTimer {
     selector;
     launch_year;
@@ -15,11 +17,12 @@ class CountDownTimer {
     launch_min;
     launch_sec;
     clock_freq;
+    reload;
     start_date;
     // Holds HTML Components
     clocks;
 
-    constructor(_selector, _launch_year, _launch_month, _launch_day, _launch_hour, _launch_min, _launch_sec, _clock_freq=1000) {
+    constructor(_selector, _launch_year, _launch_month, _launch_day, _launch_hour, _launch_min, _launch_sec, _clock_freq=1000, _reload=true) {
         this.selector = _selector;
         this.launch_year = _launch_year;
         this.launch_month = _launch_month;
@@ -28,17 +31,20 @@ class CountDownTimer {
         this.launch_min = _launch_min;
         this.launch_sec = _launch_sec;
         this.clock_freq = _clock_freq;
+        this.reload = _reload;
         this.start_date = new Date(this.launch_year, this.launch_month - 1, this.launch_day, this.launch_hour, this.launch_min, this.launch_sec);
     }
 
     Render() {
+        if (this.TimeBetween() < 0) return
         this.clocks = document.querySelectorAll(this.selector);
         setInterval(() => {
             this.clocks.forEach( clock => {
-                clock.innerHTML = this.Display();
                 if (this.TimeBetween() < 0) {
-                    // window.location.reload(true);
+                    window.location.reload(this.reload);   
+                    return
                 }
+                clock.innerHTML = this.Display();
             })
         }, this.clock_freq);
     }
@@ -68,54 +74,40 @@ class CountDownTimer {
 
     GenerateDiff() {
         let time_between = this.TimeBetween() / 1000;
-        let months = 0;
-        let weeks = 0;
-        let days = 0;
-        let hours = 0;
-        let mins = 0;
-        let secs = 0;
+        let diff = {}
         // Breaking Apart by Seconds
         const seconds_per_min = 60;
         const seconds_per_hour = 3600;
         const seconds_per_day = 60 * 60 * 24;
         const seconds_per_week = seconds_per_day * 7;
         const seconds_per_month = seconds_per_day * 30;
-
         while(time_between > 0) {
             if (time_between >= seconds_per_month) {
                 time_between -= seconds_per_month;
-                months++;
+                diff.months === undefined ? diff.months = 1 : diff.months++;
             }
             else if(time_between >= seconds_per_week) {
                 time_between -= seconds_per_week;
-                weeks++;
+                diff.weeks === undefined ? diff.weeks = 1 : diff.weeks++;
             }
             else if (time_between >= seconds_per_day) {
                 time_between -= seconds_per_day;
-                days++;
+                diff.days === undefined ? diff.days = 1 : diff.days++;
             }
             else if (time_between >= seconds_per_hour) {
                 time_between -= seconds_per_hour;
-                hours++;
+                diff.hours === undefined ? diff.hours = 1 : diff.hours++;
             }
             else if (time_between >= seconds_per_min) {
                 time_between -= seconds_per_min;
-                mins++;
+                diff.mins === undefined ? diff.mins = 1 : diff.mins++;
             }  else {
-                secs = Math.floor(time_between);
+                diff.secs = Math.floor(time_between);
                 time_between = 0;
                 break;
             }
         }
-        // Return Object 
-        return {
-            months: months,
-            weeks: weeks,
-            days: days,
-            hours: hours,
-            mins: mins,
-            secs: secs
-        };
+        return diff
     }
 
     FmtTimeStr(diff) {
